3034ae2153a4f03e5e8136615382eed9e68300ba
Hager <hagertaher257@gmail.com>
2025-08-11 22:07:59 +0300

    Green test : if minutes reached 60 it should increase hours

--- Accumulator/src/main/java/stopwatch/StopWatch.java
+++ Accumulator/src/main/java/stopwatch/StopWatch.java
@@ -2,13 +2,21 @@ package stopwatch;
 
 public class StopWatch {
     int minutes;
+    int hours ;
     public void record(int minutes) {
         if (minutes >= 0) {
             this.minutes = minutes;
+        }
+        if(this.minutes >= 60){
+            this.hours++;
+            this.minutes = this.minutes - 60;
         }
     }
 
     public int getMinutes() {
         return minutes;
+    }
+    public int getHours(){
+        return hours ;
     }
 }
