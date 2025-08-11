47cd3ff2f296a340995a04eea50245bcecae3a57
Hager <hagertaher257@gmail.com>
2025-08-11 22:40:32 +0300

    Green test : days may be increased accordinĀ to daily working hours

--- Accumulator/src/main/java/stopwatch/StopWatch.java
+++ Accumulator/src/main/java/stopwatch/StopWatch.java
@@ -4,6 +4,7 @@ public class StopWatch {
     int minutes;
     int hours ;
     int days ;
+    boolean isWorking ;
     public void record(int minutes) {
         if (minutes >= 0) {
             this.minutes = minutes;
@@ -12,10 +13,17 @@ public class StopWatch {
              this.hours = this.minutes / 60;
             this.minutes = this.minutes % 60;
         }
-        if(this.hours >= 24){
-           this.days = this.hours / 24 ;
-            this.hours = this.hours % 24 ;
-        }
+        int hoursPerDay = 24 ;
+         if(isWorking) 
+         hoursPerDay =8 ;
+
+         if(this.hours >= hoursPerDay){
+            this.days = this.hours / hoursPerDay ;
+            this.hours = this.hours % hoursPerDay ;
+         }
+    }
+    public void setIsWorking(boolean isWorking){
+        this.isWorking = isWorking ;
     }
 
     public int getMinutes() {
