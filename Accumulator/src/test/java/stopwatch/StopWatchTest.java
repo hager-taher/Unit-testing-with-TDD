6fc185c7e0cdd314666c7c4dd7787421a56235b4
Hager <hagertaher257@gmail.com>
2025-08-11 22:05:18 +0300

    Red test :if minutes reached 60 it should increase hours

--- Accumulator/src/test/java/stopwatch/StopWatchTest.java
+++ Accumulator/src/test/java/stopwatch/StopWatchTest.java
@@ -1,26 +1,48 @@
 package stopwatch;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class StopWatchTest {
 
     @Test
-    void test1() {
+    void givenStopWatch_whenRecord_thenMinutesAreValid() {
+        //Arrange
         StopWatch stopWatch = new StopWatch();
+        //Act
         stopWatch.record(14);
+        //Assert
         int minutes = stopWatch.getMinutes();
         Assertions.assertEquals(14, minutes);
     }
+      
 
     @Test
-    void test2() {
+    void givenStopWatch_whenRecordNegative_thenIgnore() {
+        //Arrange
         StopWatch stopWatch = new StopWatch();
+        //Act
         stopWatch.record(-5);
+        //Assert
         int minutes = stopWatch.getMinutes();
         if (minutes != 0) {
             Assertions.fail("Invalid value");
         }
     }
+@Test
+    void givenStopWatch_whenRecord_60_Minutes_thenIncreaseHours(){
+        // Arrange
+        StopWatch stopWatch = new StopWatch();
 
+        // Act
+        stopWatch.record(60);
+
+        // Assert
+        int minutes = stopWatch.getMinutes();
+        int hours = stopWatch.getHours();
+        assertEquals(0, minutes);
+        assertEquals(1, hours);
+    }
 }
